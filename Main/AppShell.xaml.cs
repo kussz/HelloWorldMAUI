df101bd1baffec67eccfad9815f876fb6cd29bb6
kussz <deusvultal4@gmail.com>
2025-02-02 17:42:16 +0300

    Добавьте файлы проекта.

---
+++ Main/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Main
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Main/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace Main
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            ShowCount();
+            
+        }
+
+        private void Counter2Btn_Clicked(object sender, EventArgs e)
+        {
+            count *= 2;
+            ShowCount();
+        }
+
+        private void Counter3Btn_Clicked(object sender, EventArgs e)
+        {
+            count *= 3;
+            ShowCount();
+        }
+
+        private void ShowCount()
+        {
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
